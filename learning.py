28a030c94ad6593cb16899cf8515b0f5a151b1aa
parsivishwak <parsi.vishwak@gmail.com>
2025-10-27 20:46:03 +0530

    newchanges

--- learning.py
+++ learning.py
@@ -1,2 +1,11 @@
-hoels = 'happy'
-print(hoels)ddd
+welcome = 'hello! i welcome you '
+ask = input('enter your name please ')
+askPassword = input('enter your password ')
+print(welcome)
+
+askagain = 'enter your user name'
+askpass = input('enter your password')
+if askpass == askPassword:
+    print('your wore your password correctly nice!!!!')
+else:
+    print('your wrote you password wrong thats very bad!!!')
