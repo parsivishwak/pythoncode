a0643b057eba3191cc455ad40bc82658777ddb6a
parsivishwak <parsi.vishwak@gmail.com>
2025-10-28 22:49:00 +0530

    new changes in atm simulator

--- atmsim.py
+++ atmsim.py
@@ -1,16 +1,43 @@
-askingUserName = input('Enter your username: ')
+correct_username = "vishwak"
+correct_password = "1234"
 
-# Check if username contains only letters
-if not askingUserName.isalpha():
-    print('Please enter only alphabets for username')
-else:
-    askingPassword = input('Enter your password: ')
+asking_username = input("Please enter the user name: ")
+
+
+
+if asking_username == correct_username:
+    asking_password = input("Enter your password: ")
     
-    # Check if password contains only numbers
-    if not askingPassword.isdigit():
-        print('Please enter only numbers for password')
+    if asking_password == correct_password:
+        
+
+        print("\nYou have entered the SBI bank online ATM machine.")
+        print('PLEASE CHOOSE THE ACTION YOU WOULD LIKE TO PERFORM:')
+        print('1. widraw')
+        print('2. donate')
+        
+        asking = input("Enter the option you would like to perform (widraw/donate): ").lower()
+        
+
+        if asking == 'donate':
+            donation_amount = input("How much would you like to donate? ")
+
+            print(f"Thank you for your donation of {donation_amount}!")
+            
+        elif asking == 'widraw':
+            withdrawal_amount = input('How much would you like to widraw? ')
+
+            print(f"Please take your cash of {withdrawal_amount}.")
+
+            
+        else:
+  
+            print('You have entered the wrong option. Please restart the process.')
+            
     else:
-        # Only save if both are valid
-        with open("password.txt", "a") as file:
-            file.write(askingUserName + ' : ' + askingPassword + '\n')
-        print('Username and password saved successfully!')
+
+        print('Incorrect password. Please restart the process.')
+        
+else:
+
+    print('Incorrect username. Please restart the process.')
