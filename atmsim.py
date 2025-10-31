375e289122bfc3495e42927dc17aae151e9f09fc
parsivishwak <parsi.vishwak@gmail.com>
2025-10-31 19:28:59 +0530

    spelling mistakes cleared in atm simulator

--- atmsim.py
+++ atmsim.py
@@ -13,7 +13,7 @@ if asking_username == correct_username:
 
         print("\nYou have entered the SBI bank online ATM machine.")
         print('PLEASE CHOOSE THE ACTION YOU WOULD LIKE TO PERFORM:')
-        print('1. widraw')
+        print('1. widhrew')
         print('2. donate')
         
         asking = input("Enter the option you would like to perform (widraw/donate): ").lower()
@@ -28,16 +28,12 @@ if asking_username == correct_username:
             withdrawal_amount = input('How much would you like to widraw? ')
 
             print(f"Please take your cash of {withdrawal_amount}.")
-
-            
         else:
   
             print('You have entered the wrong option. Please restart the process.')
-            
     else:
 
         print('Incorrect password. Please restart the process.')
-        
 else:
 
     print('Incorrect username. Please restart the process.')
